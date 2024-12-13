205f55c19b69ec8b87330254ac9198f7ba4c3399
canalun <i.am.kanaru.sato@gmail.com>
2024-12-13 19:28:21 +0900

    change soundcloud player

--- components/EmbeddedSoundCloudPlayer.tsx
+++ components/EmbeddedSoundCloudPlayer.tsx
@@ -2,11 +2,11 @@ export function EmbeddedSoundCloudPlayer() {
   return (
     <iframe
       width="100%"
-      height="300"
+      height="100%"
       scrolling="no"
-      frameBorder="no"
+      frameborder="no"
       allow="autoplay"
-      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1547544679&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1547544679&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
     />
   );
 }
