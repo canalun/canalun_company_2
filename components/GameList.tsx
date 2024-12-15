bf3b54b220ee997d0e79168bab67c31d3a5de3d3
canalun <i.am.kanaru.sato@gmail.com>
2024-12-15 23:54:18 +0900

    change style

--- components/GameList.tsx
+++ components/GameList.tsx
@@ -1,35 +1,59 @@
 export function GameList(props: { fontSize: string }) {
   return (
-    <ul
-      style={{
-        paddingLeft: "0",
-        marginTop: "0",
-        fontSize: `${props.fontSize}`,
-      }}
-    >
-      <li>
-        <a href="/guest_rooms/oshiriGame">
-          <strong>oshiri katori</strong>
-        </a>: for those who want summer chill feeling...!
-      </li>
-      <li>
-        <a href="/guest_rooms/reverseGame">
-          <strong>reverse game</strong>
-        </a>: irritating puzzle
-      </li>
-      <li>
-        <a href="/guest_rooms/brickBlockAnywhere">
-          <strong>brick-block-anywhere(DEMO)</strong>
-        </a>: legendary brick-block game for those who love DOM
-      </li>
-      <li>
-        <a href="./guest_rooms/hotel/index.html">
-          <strong>hotel</strong>
-        </a>: for those who like riddles…
-        <strong>
-          <span style="color:red;">[temporary closing]</span>
-        </strong>
-      </li>
-    </ul>
+    <div style={{ lineHeight: "1.2em", fontSize: `${props.fontSize}` }}>
+      <ul
+        style={{
+          paddingLeft: "0",
+        }}
+      >
+        <li
+          style={{
+            "list-style": "none",
+            marginBottom: "10px",
+          }}
+        >
+          <a href="/guest_rooms/oshiriGame">
+            <strong>oshiri katori</strong>
+          </a>
+          : for those who want summer chill feeling...!
+        </li>
+        <li
+          style={{
+            "list-style": "none",
+            marginBottom: "10px",
+          }}
+        >
+          <a href="/guest_rooms/reverseGame">
+            <strong>reverse game</strong>
+          </a>
+          : irritating puzzle
+        </li>
+        <li
+          style={{
+            "list-style": "none",
+            marginBottom: "10px",
+          }}
+        >
+          <a href="/guest_rooms/brickBlockAnywhere">
+            <strong>brick-block-anywhere(DEMO)</strong>
+          </a>
+          : legendary brick-block game for those who love DOM
+        </li>
+        <li
+          style={{
+            "list-style": "none",
+            marginBottom: "10px",
+          }}
+        >
+          <a href="./guest_rooms/hotel/index.html">
+            <strong>hotel</strong>
+          </a>
+          : for those who like riddles…
+          <strong>
+            <span style="color:red;">[temporary closing]</span>
+          </strong>
+        </li>
+      </ul>
+    </div>
   );
 }
